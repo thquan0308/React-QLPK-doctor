34c91b2b103eca1dcadf58e3ec154db6dfdf31c5
thquan0308 <tranhoangquanthq@gmail.com>
2024-12-03 14:54:29 +0700

    up

--- src/services/apiDoctor.js
+++ src/services/apiDoctor.js
@@ -2,21 +2,21 @@ import axios from "../utils/axios-customize"
 
 // doctor
 export const fetchAllDoctor = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-all-doctor?${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-all-doctor?${query}`
     return axios.get(URL_BACKEND)
 }
 export const fetchAllDoctorByID = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-doctor-by-id?${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-doctor-by-id?${query}`
     return axios.get(URL_BACKEND)
-}
+}//fetch doctor
 export const callCreateDoctor = (email, password, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId, mota, giaKhamVN, giaKhamNuocNgoai) => {
     return axios.post('/api/doctor/create-doctor', {
         email, password, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId, mota, giaKhamVN, giaKhamNuocNgoai
     })
 }
-export const updateDoctor = (_id, email, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId,roleId, mota, giaKhamVN, giaKhamNuocNgoai) => {
+export const updateDoctor = (_id, email, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId, roleId, mota, giaKhamVN, giaKhamNuocNgoai) => {
     return axios.put('/api/doctor/update-doctor', {
-        _id, email, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId,roleId, mota, giaKhamVN, giaKhamNuocNgoai
+        _id, email, firstName, lastName, address, phoneNumber, chucVuId, gender, image, chuyenKhoaId, phongKhamId, roleId, mota, giaKhamVN, giaKhamNuocNgoai
     })
 }
 export const deleteDoctor = (_id) => {
@@ -26,11 +26,11 @@ export const deleteDoctor = (_id) => {
 
 // chuyen khoa
 export const fetchAllChuyenKhoa = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-all-chuyen-khoa?${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-all-chuyen-khoa?${query}`
     return axios.get(URL_BACKEND)
 }
 export const fetchChuyenKhoaByID = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-chuyen-khoa-by-id?id=${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-chuyen-khoa-by-id?id=${query}`
     return axios.get(URL_BACKEND)
 }
 export const createChuyenKhoa = (name, description, image) => {
@@ -41,16 +41,16 @@ export const createChuyenKhoa = (name, description, image) => {
 export const deleteChuyenKhoa = (_id) => {
     return axios.delete(`/api/doctor/delete-chuyen-khoa/${_id}`)
 }
-export const updateChuyenKhoa = (_id, name, description , image) => {
+export const updateChuyenKhoa = (_id, name, description, image) => {
     return axios.put('/api/doctor/update-chuyen-khoa', {
-        _id, name, description , image
+        _id, name, description, image
     })
 }
 
 
 //  chuc vu
 export const fetchAllChucVu = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-all-chuc-vu?${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-all-chuc-vu?${query}`
     return axios.get(URL_BACKEND)
 }
 export const createChucVu = (name, description) => {
@@ -70,26 +70,26 @@ export const updateChucVu = (_id, name, description) => {
 
 // phong kham
 export const fetchAllPhongKham = (query) => {
-    const URL_BACKEND = `/api/doctor/fetch-all-phong-kham?${query}`    
+    const URL_BACKEND = `/api/doctor/fetch-all-phong-kham?${query}`
     return axios.get(URL_BACKEND)
 }
-export const createPhongKham = (name, address, description , image) => {
+export const createPhongKham = (name, address, description, image) => {
     return axios.post('/api/doctor/create-phong-kham', {
-        name, address, description , image
+        name, address, description, image
     })
 }
 export const deletePhongKham = (_id) => {
     return axios.delete(`/api/doctor/delete-phong-kham/${_id}`)
 }
-export const updatePhongKham = (_id, name, address, description , image) => {
+export const updatePhongKham = (_id, name, address, description, image) => {
     return axios.put('/api/doctor/update-phong-kham', {
-        _id, name, address, description , image
+        _id, name, address, description, image
     })
 }
 
 // fetch time
 export const fetchAllTime = () => {
-    const URL_BACKEND = `/api/doctor/fetch-all-time-gio`    
+    const URL_BACKEND = `/api/doctor/fetch-all-time-gio`
     return axios.get(URL_BACKEND)
 }
 export const fetchAllTime2 = (doctorId, date) => {
@@ -97,7 +97,7 @@ export const fetchAllTime2 = (doctorId, date) => {
     return axios.get(URL_BACKEND);
 }
 export const getTimeSlotsByDoctorAndDate = (query) => {
-    const URL_BACKEND = `/api/doctor/get-time-slots?${query}`    
+    const URL_BACKEND = `/api/doctor/get-time-slots?${query}`
     return axios.get(URL_BACKEND)
 }
 
@@ -108,8 +108,8 @@ export const addTimeKhamBenh = (date, time, _id) => {
     })
 }
 // xoa lich cu
-export const xoaLichCu = (_id) => { 
-    return axios.post('/api/doctor/delete-old-time-slots', { _id: _id }) 
+export const xoaLichCu = (_id) => {
+    return axios.post('/api/doctor/delete-old-time-slots', { _id: _id })
 }
 
 // upload hình ảnh
@@ -129,52 +129,53 @@ export const callUploadDoctorImg = (file) => {
 
 // tim ra doctor thong qua id
 export const fetchDoctorById = (id) => {
-    const URL_BACKEND = `/api/doctor/view-doctor?id=${id}`    
+    const URL_BACKEND = `/api/doctor/view-doctor?id=${id}`
     return axios.get(URL_BACKEND)
 }
 
 // hiển thị info doctor kèm theo thgian khám cho page đặt lịch khám
 export const fetchDoctorByNgayGio1 = (id, idGioKhamBenh, ngayKham) => {
-    const URL_BACKEND = `/api/doctor/page-dat-lich-kham?id=${id}&idGioKhamBenh=${idGioKhamBenh}&ngayKham=${ngayKham}`    
+    const URL_BACKEND = `/api/doctor/page-dat-lich-kham?id=${id}&idGioKhamBenh=${idGioKhamBenh}&ngayKham=${ngayKham}`
     return axios.get(URL_BACKEND)
 }
 export const fetchDoctorByNgayGio = (query) => {
-    const URL_BACKEND = `/api/doctor/page-dat-lich-kham${query}`    
+    const URL_BACKEND = `/api/doctor/page-dat-lich-kham${query}`
     return axios.get(URL_BACKEND)
 }
 
 // dat lich kham
 export const datLichKhamBenh = (
     _idDoctor, _idTaiKhoan, patientName, email,
-    gender, phone, dateBenhNhan, address, lidokham, 
+    gender, phone, dateBenhNhan, address, lidokham,
     hinhThucTT, tenGioKham, ngayKhamBenh, giaKham
-) => { 
-    const data = {_idDoctor, _idTaiKhoan, patientName, email,
-        gender, phone, dateBenhNhan, address, lidokham, 
+) => {
+    const data = {
+        _idDoctor, _idTaiKhoan, patientName, email,
+        gender, phone, dateBenhNhan, address, lidokham,
         hinhThucTT, tenGioKham, ngayKhamBenh, giaKham
     }
-    return axios.post('/api/doctor/dat-lich-kham', data) 
+    return axios.post('/api/doctor/dat-lich-kham', data)
 }
 
 // get lich kham
 export const fetchLichKham = (idKhachHang) => {
-    const URL_BACKEND = `/api/doctor/lich-hen?idKhachHang=${idKhachHang}`    
+    const URL_BACKEND = `/api/doctor/lich-hen?idKhachHang=${idKhachHang}`
     return axios.get(URL_BACKEND)
 }
 
 // tim bac si thong qua id chuyen khoa
 export const fetchDoctorByChuyenKhoa = (idChuyenKhoa) => {
-    const URL_BACKEND = `/api/doctor/doctor-chuyen-khoa?idChuyenKhoa=${idChuyenKhoa}`    
+    const URL_BACKEND = `/api/doctor/doctor-chuyen-khoa?idChuyenKhoa=${idChuyenKhoa}`
     return axios.get(URL_BACKEND)
 }
 
 export const handleHuyOrder = (query) => {
-    const URL_BACKEND = `/api/doctor/huy-order?idHuy=${query}`    
+    const URL_BACKEND = `/api/doctor/huy-order?idHuy=${query}`
     return axios.post(URL_BACKEND)
 }
 
 export const findAllLichHen = (query) => {
-    const URL_BACKEND = `/api/doctor/find-all-order?${query}`    
+    const URL_BACKEND = `/api/doctor/find-all-order?${query}`
     return axios.get(URL_BACKEND)
 }
 
