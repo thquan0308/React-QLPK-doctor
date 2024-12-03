a94b9381dedd5bd5407cac312c6f7c7e53ec1a7e
thquan0308 <tranhoangquanthq@gmail.com>
2024-12-03 16:50:48 +0700

    change host for doctor FE dr

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
-// https://vitejs.dev/config/server-options.html#server-options
+
+// Thiết lập thứ tự kết quả DNS
 dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3001,  // Cấu hình cổng 3001
+    open: '/login-doctor',  // Mở ứng dụng với URL /login-doctor
   },
+  base: '/login-doctor/',  // Cấu hình base path cho ứng dụng là /login-doctor
 })
